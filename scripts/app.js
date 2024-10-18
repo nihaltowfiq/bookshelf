4137cd95d3e149ecb668b88088e7853fa1db6d7e
nihaltowfiq <nihaltowfiq@gmail.com>
2024-10-18 23:52:23 +0600

    persist the actions data, added animation and removed extra files

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,10 @@
+const query = storage('session').get('query') || {};
+
 const perPage = 32;
-let currentSearch = '';
-let currentGenre = '';
-let currentPage = 1;
 let totalPage = 1;
+let currentSearch = query?.search ?? '';
+let currentFilter = query?.filter ?? '';
+let currentPage = query?.page ?? 1;
 
 document.addEventListener('DOMContentLoaded', () => {
   if (getPage() === 'wishlist') {
@@ -19,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     handleSearch();
     handleGenreFilter();
     displayFilter(filterData);
+    updateQueryOnInterface();
   }
 
   displayActivePage();
@@ -65,27 +68,34 @@ async function getBook(bookId) {
   displayBookDetails(data?.['results']?.[0]);
 }
 
-async function loadBooks(page = currentPage, search = currentSearch, genre) {
-  const data = await fetchBooks({ page, search, genre });
+async function loadBooks() {
+  const data = await fetchBooks({
+    page: currentPage,
+    search: currentSearch,
+    genre: currentFilter,
+  });
 
   if (!data) return;
 
-  const finalData = getFinalData(data?.results);
+  const finalData = addWishlistStatus(data?.results);
 
   totalPage = Math.ceil(data.count / perPage);
 
   displayBooks(finalData);
-  storeBook(finalData);
 
-  if (data.count > 0) {
-    displayPagination(page);
-  } else {
-    clearPagination();
-  }
+  // setTimeout: to match with book-list animation
+  setTimeout(() => {
+    if (data.count > 0) {
+      displayPagination(currentPage);
+    } else {
+      clearPagination();
+    }
+  }, 600);
 }
 
-function getFinalData(data) {
+function addWishlistStatus(data) {
   if (!data) return null;
+
   const wishlist = storage().get('wishlist');
   const final_data = data.map((el) => ({
     ...el,
@@ -95,7 +105,7 @@ function getFinalData(data) {
   return final_data;
 }
 
-function storeBook(data) {
+function setBookList(data) {
   storage('session').set('book-list', data);
 }
 
@@ -103,7 +113,11 @@ function handleSearch() {
   const searchInput = document.getElementById('search-input');
 
   const debouncedSearch = debounce((value) => {
-    loadBooks(1, value);
+    currentSearch = value;
+    currentPage = 1;
+    setQuery('search', value);
+    setQuery({ page: 1, search: value });
+    loadBooks();
   }, 500);
 
   searchInput.addEventListener('input', () => {
@@ -112,10 +126,8 @@ function handleSearch() {
       window.getComputedStyle(clearBtn).visibility === 'hidden';
 
     if (searchInput.value.trim() !== '' && isBtnHidden) {
-      console.log('inside if');
       clearBtn.style.visibility = 'visible';
     } else if (searchInput.value.trim() === '' && !isBtnHidden) {
-      console.log('inside else');
       clearBtn.style.visibility = 'hidden';
     }
 
@@ -127,6 +139,7 @@ function clearSearch() {
   const clearBtn = document.querySelector('.search-bar .close-btn');
   const searchInput = document.getElementById('search-input');
   searchInput.value = '';
+  setQuery({ search: searchInput.value });
   clearBtn.style.visibility = 'hidden';
   loadBooks();
 }
@@ -145,7 +158,10 @@ function debounce(fn, delay) {
 function handleGenreFilter() {
   const filter = document.getElementById('filter');
   filter.addEventListener('change', () => {
-    loadBooks(1, '', filter.value);
+    currentFilter = filter.value;
+    currentPage = 1;
+    setQuery({ page: 1, filter: filter.value });
+    loadBooks();
   });
 }
 
@@ -185,16 +201,13 @@ const paginationRanges = (currentPage, totalPage) => {
 const changePage = (page) => {
   if (page < 1 || page > totalPage) return;
   currentPage = page;
-  loadBooks(page);
+  setQuery({ page });
+  loadBooks();
 };
 
 function toggleWishlist(bookId) {
-  console.log({ bookId });
-
   let books = storage('session').get('book-list');
   let book = books.find((el) => bookId === el.id);
-
-  console.log(book);
 
   let wishlist = storage().get('wishlist') || [];
 
@@ -222,4 +235,55 @@ function toggleWishlist(bookId) {
   } else {
     displayBooks(books);
   }
+}
+
+function setQuery(queries) {
+  const query = storage('session').get('query');
+
+  storage('session').set('query', { ...query, ...queries });
+}
+
+function updateQueryOnInterface() {
+  const searchInput = document.getElementById('search-input');
+  const filter = document.getElementById('filter');
+
+  searchInput.value = currentSearch;
+  filter.value = currentFilter;
+
+  if (currentSearch?.trim()?.length > 0) {
+    const clearBtn = document.querySelector('.search-bar .close-btn');
+    clearBtn.style.visibility = 'visible';
+  }
+}
+
+function storage(type = 'local') {
+  const store = type === 'session' ? sessionStorage : localStorage;
+
+  const setValue = (key, value) => {
+    if (!key || !value) return;
+
+    store.setItem(key, JSON.stringify(value));
+  };
+
+  const getValue = (key) => {
+    if (!key) return;
+
+    const item = store.getItem(key);
+    if (item) {
+      return JSON.parse(item);
+    }
+    return null;
+  };
+
+  const removeValue = (key) => {
+    if (!key) return;
+
+    store.removeItem(key);
+  };
+
+  return {
+    set: setValue,
+    get: getValue,
+    remove: removeValue,
+  };
 }
--- scripts/dom.js
+++ scripts/dom.js
@@ -5,52 +5,62 @@ const pagination = document.getElementById('pagination');
 const filter = document.getElementById('filter');
 
 function displayBooks(books) {
-  bookList.innerHTML = '';
+  // Fade out the current list of books
+  const oldBooks = document.querySelectorAll('.book-card');
+  oldBooks.forEach((book) => {
+    book.classList.add('fade-out');
+    setTimeout(() => {
+      book.remove();
+    }, 500);
+  });
 
-  if (!books?.length)
-    bookList.innerHTML = `
+  // Wait for the fade-out animation to complete before adding new books
+  setTimeout(() => {
+    if (!books?.length) {
+      bookList.innerHTML = `
         <p style="margin:2rem 0; text-align:center; color:var(--primary-light); font-weight:600">
           No books found!
         </p>
       `;
+    } else {
+      books.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
 
-  books.forEach((book) => {
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
+        bookCard.innerHTML = `
+          <div class="img-wrapper">
+            <img src="${book.formats['image/jpeg']}" alt="${book.title}">
+            <button title="Wishlist" class="wishlist" onclick="toggleWishlist(${
+              book.id
+            })">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="18" width="18" fill="red">
+                <path d="${
+                  book?.isWishlist
+                    ? 'M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853Z'
+                    : 'M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853ZM18.827 6.1701C17.3279 4.66794 14.9076 4.60701 13.337 6.01687L12.0019 7.21524L10.6661 6.01781C9.09098 4.60597 6.67506 4.66808 5.17157 6.17157C3.68183 7.66131 3.60704 10.0473 4.97993 11.6232L11.9999 18.6543L19.0201 11.6232C20.3935 10.0467 20.319 7.66525 18.827 6.1701Z'
+                }"></path></svg>
+            </button>
+            <div class="id">
+              <span>ID: </span>
+              <span>${book.id}</span>
+            </div>
+          </div>
+          <div class="detail">
+            <h4 class="title" title="${book.title}">
+              <a href="${`./book.html?book=${book.id}`}">
+                ${book.title}
+              </a>
+            </h4>        
+          </div>
+          <p class="author"><span>Author:</span> ${book.authors
+            .map((a) => a.name)
+            .join(', ')}</p>
+        `;
 
-    bookCard.innerHTML = `
-      <div class="img-wrapper">
-        <img src="${book.formats['image/jpeg']}" alt="${book.title}">
-        <button title="Wishlist" class="wishlist" onclick="toggleWishlist(${
-          book.id
-        })">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="18" width="18" fill="red"><path d="${
-            book?.isWishlist
-              ? 'M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853Z'
-              : 'M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853ZM18.827 6.1701C17.3279 4.66794 14.9076 4.60701 13.337 6.01687L12.0019 7.21524L10.6661 6.01781C9.09098 4.60597 6.67506 4.66808 5.17157 6.17157C3.68183 7.66131 3.60704 10.0473 4.97993 11.6232L11.9999 18.6543L19.0201 11.6232C20.3935 10.0467 20.319 7.66525 18.827 6.1701Z'
-          }"></path></svg>
-        </button>
-
-        <div class="id">
-          <span>ID: </span>
-          <span>${book.id}</span>
-        </div>
-      </div>
-      <div class="detail">
-        <h4 class="title" title="${book.title}">
-          <a href="${`./book.html?book=${book.id}`}">
-            ${book.title}
-          </a>
-        </h4>        
-      </div>
-
-      <p class="author"><span>Author:</span> ${book.authors
-        .map((a) => a.name)
-        .join(', ')}</p>
-      
-    `;
-    bookList.appendChild(bookCard);
-  });
+        bookList.appendChild(bookCard);
+      });
+    }
+  }, 700);
 }
 
 function displayFilter(data) {
